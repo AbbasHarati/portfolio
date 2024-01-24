7a039803c1d467e84f28f0bfb1a7a93e1eef26bf
AbbasHarati <harati.ab@gmail.com>
2024-01-24 21:39:28 +0330

    some edit on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,9 +1,12 @@
-
 export default function contactPage() {
-    return (
-        <>
-        <div className="m-2">Contact page</div>
-        <input type='text' className="border-2 m-1" />
-        </>
-    )
+  return (
+    <>
+      <div className="m-2">Contact page</div>
+      <form className="flex flex-col">
+        <input type="text" className="border-2 m-1 rounded" />
+        <input type="text" className="border-2 m-1 rounded" />
+        <button className="bg-blue-500 rounded-lg">ارسال</button>
+      </form>
+    </>
+  );
 }
